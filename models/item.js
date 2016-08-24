316e84507e3c6d4dba4c17b19fe5a677b85feaff
Justin Doak <justinhdoak@gmail.com>
2016-08-24 09:25:16 -0400

    More work on inventory routing. Added node.js version requirement to package.json for Heroku deployment.

--- models/item.js
+++ models/item.js
@@ -3,13 +3,13 @@ var mongoose = require('mongoose');
 var ItemSchema = new mongoose.Schema({
   name:           { type: String, required: true },
   brand:          { type: String },
-  model:          { type: String },
+  itemModel:      { type: String },
   serial:         { type: String },
   category:       { type: String, required: true },
   purchaseDate:   { type: String },
   pricePaid:      { type: String },
   estimatedValue: { type: String },
-  details:        { type: String },
+  otherNotes:     { type: String },
   picture:        { type: String },
   receipt:        { type: String },
   home:           { type: mongoose.Schema.ObjectId, ref: 'Home' }
--- routes/inventory.js
+++ routes/inventory.js
@@ -1,10 +1,75 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
+var Item    = require('../models/item');
+var User    = require('../models/user');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+function makeError(res, message, status) {
+  res.statusCode = status;
+  var error = new Error(message);
+  error.status = status;
+  return error;
+}
+
+function authenticate(req, res, next) {
+  if(!req.isAuthenticated()) {
+    req.flash('error', 'Please signup or login.');
+    res.redirect('/');
+  }
+  else {
+    next();
+  }
+}
+
+/* GET inventory home page. */
+router.get('/:homeid', authenticate, function(req, res, next) {
+  var home = currentUser.homes.id(req.params.homeid);
+  res.render('inventory/index', { home: home, message: req.flash() });
 });
+
+
+//  User.homes.findOne({ _id: req.params.homeid })
+//  .populate('inventory')
+//  .exec(function (err, home) {
+//    if (err) return handleError(err);
+//    console.log('populatedhome:', home);
+//    res.render('inventory/index', { home: home, message: req.flash() });
+//  });
+//});
+
+
+// Get new item page
+router.get('/:homeid/new', authenticate, function(req, res, next) {
+  var home = currentUser.homes.id(req.params.homeid);
+  res.render('inventory/new', { home: home, message: req.flash() });
+});
+
+// Create new item
+router.post('/:homeid', authenticate, function(req, res, next) {
+  var item = new Item ({
+    name           : req.body.name,
+    brand          : req.body.brand,
+    itemModel      : req.body.itemModel,
+    serial         : req.body.serial,
+    category       : req.body.category,
+    purchaseDate   : req.body.purchaseDate,
+    pricePaid      : req.body.pricePaid,
+    estimatedValue : req.body.estimatedValue,
+    otherNotes     : req.body.otherNotes,
+    home           : req.params.homeid
+  });
+  Item.create(item)
+  .then(function(createdItem) {
+    currentUser.homes.id(req.params.homeid).inventory.push(createdItem._id);
+    return currentUser.save();
+  })
+  .then(function() {
+    var home = currentUser.homes.id(req.params.homeid);
+    res.render('inventory/index', { home: home, message: req.flash() });
+  }, function(err) {
+    return next(err);
+  });
+});
+
 
 
 module.exports = router;
