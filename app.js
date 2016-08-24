2d1bb0ccc7c30740c8310ff88fe518154cb62b47
Justin Doak <justinhdoak@gmail.com>
2016-08-24 09:47:58 -0400

    Updated DB connection code to use MongoLab when in Production

--- app.js
+++ app.js
@@ -20,7 +20,20 @@ var inventoryRouter = require('./routes/inventory');
 var app = express();
 
 // Connect to database
-mongoose.connect('mongodb://localhost/insurance-inventory');
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect('mongodb://localhost/express-movies');
+}
+mongoose.connection.on('error', function(err) {
+  console.error('MongoDB connection error: ' + err);
+  process.exit(-1);
+  }
+);
+mongoose.connection.once('open', function() {
+  console.log("Mongoose has connected to MongoDB!");
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
