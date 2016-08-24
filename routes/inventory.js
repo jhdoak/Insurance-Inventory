6862faaa67d77ead80453f768dd59db34330f809
Justin Doak <justinhdoak@gmail.com>
2016-08-24 14:09:27 -0400

    Fixed redirect issues.

--- routes/inventory.js
+++ routes/inventory.js
@@ -35,7 +35,7 @@ router.get('/:homeid/new', authenticate, function(req, res, next) {
 
 // Create new item
 router.post('/:homeid', authenticate, function(req, res, next) {
-  var item = new Item ({
+  var item = ({
     name           : req.body.name,
     brand          : req.body.brand,
     itemModel      : req.body.itemModel,
@@ -49,8 +49,7 @@ router.post('/:homeid', authenticate, function(req, res, next) {
   currentUser.homes.id(req.params.homeid).inventory.push(item);
   currentUser.save()
   .then(function() {
-    var home = currentUser.homes.id(req.params.homeid);
-    res.redirect('inventory/:homeid', { home: home, items: home.inventory, message: req.flash() });
+    res.redirect('/inventory/' + req.params.homeid);
   }, function(err) {
     return next(err);
   });
@@ -70,5 +69,42 @@ router.get('/:homeid/item/:itemid/edit',  authenticate, function(req, res, next)
   res.render('inventory/edit', { home: home, item: item, message: req.flash() });
 })
 
+// Update item
+router.put('/:homeid/item/:itemid', authenticate, function(req, res, next) {
+  var home = currentUser.homes.id(req.params.homeid);
+  var item = currentUser.homes.id(req.params.homeid).inventory.id(req.params.itemid);
+  if (!home || !item) return next(makeError(res, 'Document not found', 404));
+  else {
+    item.name           = req.body.name;
+    item.brand          = req.body.brand;
+    item.itemModel      = req.body.itemModel;
+    item.serial         = req.body.serial;
+    item.category       = req.body.category;
+    item.purchaseDate   = req.body.purchaseDate;
+    item.pricePaid      = req.body.pricePaid;
+    item.estimatedValue = req.body.estimatedValue;
+    item.otherNotes     = req.body.otherNotes;
+    currentUser.save()
+    .then(function(savedUser) {
+      res.redirect('/inventory/' + req.params.homeid + '/item/' + req.params.itemid);
+    }, function(err) {
+      return next(err);
+    });
+  }
+});
+
+// Delete item
+router.delete('/:homeid/item/:itemid', authenticate, function(req, res, next) {
+  var item = currentUser.homes.id(req.params.homeid).inventory.id(req.params.itemid);
+  if (!item) return next(makeError(res, 'Document not found', 404));
+  var index = currentUser.homes.id(req.params.homeid).inventory.indexOf(item);
+  currentUser.homes.id(req.params.homeid).inventory.splice(index, 1);
+  currentUser.save()
+  .then(function(saved) {
+    res.redirect('/inventory/' + req.params.homeid);
+  }, function(err) {
+    return next(err);
+  });
+});
 
 module.exports = router;
